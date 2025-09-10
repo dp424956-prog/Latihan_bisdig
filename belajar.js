c840716c1f3e0cd793c2a64b15781a3967ef4893
dimas prayoga <dp424956@gmail.com>
2025-09-10 14:42:29 +0700

    belajar jawa script

---
+++ belajar.js
@@ -0,0 +1,12 @@
+window.onload = function() {
+     alert('Selamat datang di website Muhammad Alvini
+};
+confirm("Apakah Anda ingin melanjutkan?");
+prompt("Saya Belajar:");
+Date(10)
+parseInt("123")
+setTimeout(function() {
+     alert("saya Pemula.");
+}, 2000);
+function foo() {
+alert("Our function just ran!");
